374814f0a315b417e723d1e5307f7169d149b3d5
Collin Clark <crclark96@gmail.com>
2017-04-12 20:44:08 -0400

    add Game.java and full test suite

--- Game2Boat/src/Model/Game.java
+++ Game2Boat/src/Model/Game.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class Game {
-
+	int score = 0;
+	int time = 120; // two minutes?
+	
+	public void decreaseScore(){
+		score = (score > 0) ? score - 1 : score; // don't decrement 
+	}											//score below zero
+	public void increaseScore(){
+		score++;
+	}
+	public void decreaseTime(){
+		time = (time > 0) ? time - 1 : time; // doesn't decrement
+	}										// time below zero
+	public int getScore(){
+		return score;
+	}
+	public int getTime(){
+		return time;
+	}
 }
---
+++ Game2Boat/src/Model/GameTest.java
@@ -0,0 +1,31 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void scoreTest() {
+		Game g1 = new Game();
+		assertEquals(g1.getScore(), 0);
+		g1.decreaseScore();
+		assertEquals(g1.getScore(), 0); // check underflow
+		g1.increaseScore();
+		assertEquals(g1.getScore(), 1);
+		g1.decreaseScore();
+		assertEquals(g1.getScore(), 0);
+	}
+	@Test
+	public void timeTest(){
+		Game g1 = new Game();
+		for (int i = 120; i >= 0; i--){
+			assertEquals(g1.getTime(), i);
+			g1.decreaseTime();
+		}
+		g1.decreaseTime();
+		assertEquals(g1.getTime(), 0);  // check underflow
+	}
+
+}
