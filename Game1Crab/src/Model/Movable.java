07c4477cea601c657392ff597989bbc1df8797e3
zirons1 <zirons@udel.edu>
2017-04-14 14:40:35 -0400

    Now I am?

---
+++ Game1Crab/src/Model/Movable.java
@@ -0,0 +1,13 @@
+package Model;
+
+public interface Movable {
+	public void setyLoc(int yLoc);
+	public int getyLoc();
+	public void setxLoc(int xLoc);
+	public int getxLoc();
+	public void setYVel(int yVel);
+	public int getYVel();
+	public int getXVel();
+	public int getYAcc();
+	public void update();
+}
