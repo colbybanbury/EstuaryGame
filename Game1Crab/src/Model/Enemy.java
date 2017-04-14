db5e15c32f843901cb265b0d4d965525175ef2a0
zirons1 <zirons@udel.edu>
2017-04-14 14:43:43 -0400

    edits

---
+++ Game1Crab/src/Model/Enemy.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Enemy extends Mover{
+	
+	void remove(){
+		//Animation based
+		//This is only when there is a collision
+	}
+}
---
+++ Game1Crab/src/Model/Mover.java
@@ -0,0 +1,48 @@
+package Model;
+public abstract class Mover implements Movable{
+	public int imgHeight;
+	public int imgWidth;
+	public int yLoc;
+	public int xLoc;
+	public int yVel;
+	public int xVel;
+	public int yAcc;
+	
+	public void setyLoc(int yLoc){
+		this.yLoc = yLoc;
+	}
+	
+	public int getyLoc(){
+		return this.yLoc;
+	}
+	
+	public void setxLoc(int xLoc){
+		this.xLoc = xLoc;
+	}
+
+	public int getxLoc(){
+		return this.xLoc;
+	}
+	
+	public void setYVel(int yVel){
+		this.yVel = yVel;
+	}
+	
+	public int getYVel(){
+		return this.yVel;
+	}
+
+	public int getXVel(){
+		return this.xVel;
+	}
+	
+	public int getYAcc(){
+		return this.yAcc;
+	}
+	
+	public void update(){
+		this.xLoc += xVel;
+		this.yVel += yAcc;
+		this.yLoc += yVel;
+	}
+}
