5ef5692579e54d04797d2f7949c88622f2b03b8a
colbybanbury <cbanbury@udel.edu>
2017-04-14 11:49:56 -0400

    Fixed some of my issues and got back to my previous point

--- Game2Boat/src/model/Board.java
+++ Game2Boat/src/model/Board.java
@@ -1,5 +1,53 @@
 package model;
 
-public class Board {
+import java.util.ArrayList;
 
+public class Board {
+	private int height;
+	private int width;
+	private int outerDiameter;
+	private int innerDiameter;
+	static ArrayList<Estuary> estuaries = new ArrayList<Estuary>();
+	static int[][] boardGrid;
+	
+	public Board(int h, int w, int outer, int inner){
+		this.height = h;
+		this.width = w;
+		this.outerDiameter = outer;
+		this.innerDiameter = inner;
+		this.boardGrid = new int[width][height];
+	}
+	
+	public static ArrayList<Estuary> getEstuaries() {
+		return estuaries;
+	}
+
+	public static void setEstuaries(ArrayList<Estuary> estuaries) {
+		Board.estuaries = estuaries;
+	}
+
+	public static int[][] getBoardGrid() {
+		return boardGrid;
+	}
+
+	public static void setBoardGrid(int[][] boardGrid) {
+		Board.boardGrid = boardGrid;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getOuterDiameter() {
+		return outerDiameter;
+	}
+
+	public int getInnerDiameter() {
+		return innerDiameter;
+	}
+	
 }
