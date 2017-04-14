a679c4737fdac7affb0d7af989ca2dc5b686f7ef
colbybanbury <cbanbury@udel.edu>
2017-04-14 12:31:26 -0400

    added a test file for game 1

---
+++ Game1Crab/src/test/PlayerTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+	
+	@Test
+	public void test() {
+		Player p = new player();
+		fail("Not yet implemented");
+	}
+
+}
