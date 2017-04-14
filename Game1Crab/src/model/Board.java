7129920d6038df948bfa3520a286788536cd4020
Collin Clark <crclark96@gmail.com>
2017-04-14 00:13:59 -0400

    add most of board class, add used methods to movable

--- Game1Crab/src/model/Board.java
+++ Game1Crab/src/model/Board.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Board {
+	int width;
+	int height;
+	Player player;
+	List<Enemy> enemies = new ArrayList<Enemy>();
+	int[][] scentTrail;
+	int progress;
+	Board(int width, int height){
+		this.width = width;
+		this.height = height;
+		scentTrail = new int[width][height];
+		progress = 0;
+		player = new Player();
+	}
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public void checkSalinity(){}
+	// iterates through enemies on board to check for collisions with
+	// player
+	public boolean checkCollision(){   // changed from UML
+		for (Enemy enemy: enemies){
+			if (enemy.getXLoc() <= player.getXLoc() + player.getImgWidth()){
+				return true;
+			}
+		}
+		return false;
+	}
+	void drought(){}
+	void storm(){}
+	void construction(){}
 }
