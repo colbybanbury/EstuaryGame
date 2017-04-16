4b215d33565f3279e31555193f7587f9b93b56e9
colbybanbury <cbanbury@udel.edu>
2017-04-15 23:18:05 -0400

    Implemented the player class. Might was to decrease the number of parameters for the crabs with set values.

--- Game1Crab/src/model/Player.java
+++ Game1Crab/src/model/Player.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Player extends Mover{
+	final int jumpInc = 5; //this is how quickly the crab moves up after jumping
+	
+	public Player( int imgH, int imgW,int yL, int xL, int yV, int xV, int yA, Rectangle loc){
+		this.imgHeight = imgH;
+		this.imgWidth = imgW;
+		this.yLoc = yL;
+		this.xLoc = xL;
+		this.yVel = yV;
+		this.xVel = xV;
+		this.yAcc = yA;
+		this.location = loc;
+	}
+	
+	//when the jump button is pressed the crabs yVelocity is increased by a set amount
+	public void jump(){	
+		this.yVel += jumpInc;
+	}
 	
 }
