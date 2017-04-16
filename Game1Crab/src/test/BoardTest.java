175b0f538a5ada7acc44e85bbfe00d13c8db1a18
Chris Clayton <chrisbme@udel.edu>
2017-04-16 01:39:55 -0400

    basic drought and storm tests added

--- Game1Crab/src/test/BoardTest.java
+++ Game1Crab/src/test/BoardTest.java
@@ -13,4 +13,42 @@ public class BoardTest {
 		assertEquals(b1.getHeight(), 800);
 	}
 
+	@Test
+	//TODO
+	public void checkSalinityTest() {
+		
+	}
+	
+	@Test
+	//TODO
+	public void checkCollisionTest() {
+		
+	}
+	
+	@Test
+	public void droughtTest(){
+		Board b2 = new Board(400, 800);
+		b2.drought();
+		assertEquals((((b2.getHeight())/3)/2), 133); //assertEquals on (((getHeight())/3)/2) due to truncation
+	}
+	
+	@Test
+	public void stormTest(){
+		Board b3 = new Board(400, 800);
+		b3.storm();
+		assertEquals(b3.wavyFactor, 10);
+	}
+	
+	@Test
+	//TODO
+	public void constructionTest(){
+		
+	}
+	
+	@Test
+	//TODO
+	public void updateTest(){
+		
+	}
+	
 }
