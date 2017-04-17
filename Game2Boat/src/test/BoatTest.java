5e4649e1e49714e319249f689612db13be836bda
Collin Clark <crclark96@gmail.com>
2017-04-16 20:47:36 -0400

    add more Boat tests

--- Game2Boat/src/test/BoatTest.java
+++ Game2Boat/src/test/BoatTest.java
@@ -7,15 +7,17 @@ import org.junit.Test;
 public class BoatTest {
 
 	int accel = -1;
-	int mSpeed = 2;
+	int mSpeed = 4;
 	int speedInc = 1;
 	int xLoc, speed;
 	@Test
 	public void throttleTest() {
 		Boat b1 = new Boat(accel, speedInc, mSpeed);
-		for (int i = 0; i < 15; i++){
+		for (int i = 0; i < 8; i++){
 			speed = b1.getSpeed();
 			b1.throttle();
+			System.out.println("xLoc: " + b1.getXLoc() + " Speed: " + b1.getSpeed() 
+								+ " Accel: " + b1.getAcceleration());
 			assertTrue((b1.getSpeed() == speed + speedInc) || b1.getSpeed() == b1.getMaxSpeed());
 			assertTrue(b1.getSpeed() <= b1.getMaxSpeed());
 		}
@@ -23,6 +25,12 @@ public class BoatTest {
 	@Test
 	public void moveTest(){
 		Boat b1 = new Boat(accel, speedInc, mSpeed);
+		xLoc = b1.getXLoc();
+		speed = b1.getSpeed();
+		b1.move();
+		assertEquals(speed + xLoc, b1.getXLoc());
+		assertTrue((b1.getSpeed()== b1.getAcceleration() + speed) || (b1.getSpeed() == 0));
+		b1.throttle();
 		xLoc = b1.getXLoc();
 		speed = b1.getSpeed();
 		b1.move();
