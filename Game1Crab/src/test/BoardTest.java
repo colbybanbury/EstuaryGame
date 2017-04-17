a0a403c2dcb0ac061b59391718fb66d2490c9784
Chris Clayton <chrisbme@udel.edu>
2017-04-17 15:52:39 -0400

    BoardTest updates

--- Game1Crab/src/test/BoardTest.java
+++ Game1Crab/src/test/BoardTest.java
@@ -3,6 +3,8 @@ package test;
 import static org.junit.Assert.*;
 import model.Board;
 import org.junit.Test;
+import model.Player;
+import model.Enemy;
 
 public class BoardTest {
 
@@ -21,27 +23,59 @@ public class BoardTest {
 	}
 	
 	@Test
+	public void checkCollisionTest1() { //Player same coords as Enemy
+		Board b1 = new Board(400, 800);
+		Player p1 = new Player(b1);
+		p1.setXLoc(50);
+		p1.setYLoc(50);
+		Enemy e1 = new Enemy(b1);
+		e1.setXLoc(50);
+		e1.setYLoc(50);
+		assertTrue(b1.checkCollision());
+	}
+	
+	@Test
 	//TODO
-	public void checkCollisionTest() {
-		//Need setters and getters for enemy and player location
+	public void checkCollisionTest2() {
+		Board b1 = new Board(400, 800);
+		Player p1 = new Player(b1);
+		p1.setXLoc(50);
+		p1.setYLoc(50);
+		Enemy e1 = new Enemy(b1);
+		e1.setXLoc(?);
+		e1.setYLoc(?);
+		assertTrue(b1.checkCollision());
+	}
+	
+	@Test
+	//TODO
+	public void checkCollisionTest3() {
+		Board b1 = new Board(400, 800);
+		Player p1 = new Player(b1);
+		p1.setXLoc(50);
+		p1.setYLoc(50);
+		Enemy e1 = new Enemy(b1);
+		e1.setXLoc(?);
+		e1.setYLoc(?);
+		assertTrue(b1.checkCollision());
 	}
 	
 	@Test
 	public void droughtTest(){
-		Board b2 = new Board(400, 800);
-		b2.drought();
+		Board b1 = new Board(400, 800);
+		b1.drought();
 		//TODO
 		//Need getter for scent trail height
-		assertEquals(b2.getScentTrailHeight(), 133); //Height/3/2 results in truncation, so 133
+		assertEquals(b1.getScentTrailHeight(), 133); //Height/3/2 results in truncation, so 133
 	}
 	
 	@Test
 	public void stormTest(){
-		Board b3 = new Board(400, 800);
-		b3.storm();
+		Board b1 = new Board(400, 800);
+		b1.storm();
 		//TODO
 		//Need getter for wavyFactor
-		assertEquals(b3.getWavyFactor(), 10);
+		assertEquals(b1.getWavyFactor(), 10);
 	}
 	
 	@Test
