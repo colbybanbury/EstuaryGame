06d77677bb7a45e75d7148805db8ddfaf97d785f
colbybanbury <cbanbury@udel.edu>
2017-04-17 15:22:00 -0400

    added the player tests

--- Game1Crab/src/test/PlayerTest.java
+++ Game1Crab/src/test/PlayerTest.java
@@ -9,7 +9,16 @@ public class PlayerTest {
 	@Test
 	public void test() {
 		Player p = new player();
-		fail("Not yet implemented");
+		assertEqual(p.getyAcc, -0.8);
+		assertTrue(p.getLocation instanceof Rectangle)
+	}
+	
+	@Test
+	public void jumpTest(){
+		Player p = new player();
+		int yVel = p.getYVel();
+		p.jump();
+		assertTrue(yVel+5 == p.getYVel());
 	}
 
 }
