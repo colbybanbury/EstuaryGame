fd9255aebb786be4e924672f3fc0beb3ee87bca2
zirons1 <zirons@udel.edu>
2017-04-17 14:18:16 -0400

    updated friend constructor

--- Game1Crab/src/model/Friend.java
+++ Game1Crab/src/model/Friend.java
@@ -2,4 +2,11 @@ package model;
 
 public class Friend extends Mover{
 	
+	public Friend(Board b){ 
+		this.yVel = 0;
+		this.xVel = 2; //TODO: We need to decide how fast these guys go
+		this.yAcc = 0;
+		//Friends start down in the bottom right corner and move across, carrying a fun fact along with them
+		this.location = new Rectangle(b.width, (b.height - 300), 300, 300); // Rectangle(int x, int y, int width, int height)
+	}
 }
