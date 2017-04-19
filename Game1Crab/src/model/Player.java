87686030211e853142b12de7b30569e2f9a8aa0c
Collin Clark <crclark96@gmail.com>
2017-04-18 23:43:20 -0400

    HAHAHAHA its Zach on COllin's computer

--- Game1Crab/src/model/Player.java
+++ Game1Crab/src/model/Player.java
@@ -7,13 +7,13 @@ public class Player extends Mover{
 	public Player(Board b){ 
 		this.yVel = 0;
 		this.xVel = 0;
-		this.location = new Rectangle((b.width / 10), (b.height / 2), 243, 119); // Rectangle(int x, int y, int width, int height)
-		this.yAcc = 0.8;
+		this.location = new Rectangle((b.width / 10), (b.height / 4), 243, 119); // Rectangle(int x, int y, int width, int height)
+		this.yAcc = 2.1;
 	}
 	
 	//when the jump button is pressed the crabs yVelocity is increased by a set amount
 	public void jump(){	
-		yVel = 20;
+		yVel = -24;
 	}
 
 }
