f391bcf4f48e07ced8eaa9cf0755143b9a6cd509
Collin Clark <crclark96@gmail.com>
2017-04-26 22:51:15 -0400

    fix PlayerTest to pass all tests

--- Game1Crab/src/test/PlayerTest.java
+++ Game1Crab/src/test/PlayerTest.java
@@ -13,7 +13,7 @@ public class PlayerTest {
 	@Test
 	public void test() {
 		Board b = new Board(400,800);
-		Mover p = new Player(b);
+		Mover p = b.player;
 		//assertEquals(-0.8, p.getYAcc()); I've been fooling with the values so this will be wrong for a while and I don't want errors
 		assertTrue(p.getLocation() instanceof Rectangle);
 	}
@@ -21,10 +21,13 @@ public class PlayerTest {
 	@Test
 	public void jumpTest(){
 		Board b = new Board(400,800);
-		Player p = new Player(b);
-		double yVel = p.getYVel();
+		Player p = b.player;
+		p.setXLoc(40);
 		p.jump();
-		assertEquals(yVel+5, p.getYVel());
+		assertEquals(-30, p.getYVel(), 0);
+		p.setYLoc(-40);
+		p.setYVel(0);
+		p.jump();
+		assertEquals(0, p.getYVel(), 0);
 	}
-
 }
