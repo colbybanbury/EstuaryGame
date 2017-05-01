5fcd826f7403c7a006509afa390b60a293614c9c
Collin Clark <crclark96@gmail.com>
2017-05-01 10:35:19 -0400

    add tests to MoverTest in game 1

--- Game1Crab/src/test/MoverTest.java
+++ Game1Crab/src/test/MoverTest.java
@@ -4,16 +4,18 @@ import static org.junit.Assert.*;
 import model.Board;
 import model.Friend;
 import model.Mover;
+import model.Player;
 
 import org.junit.Test;
 
 public class MoverTest {
 
 	@Test
-	public void updateTest() {
+	public void updateTest1() {
 		Board b = new Board(1000,1000);
 		Mover m = new Friend(b);
 		m.setStarted(false);
+		assertFalse(m.getStarted());
 		int yLoc = m.getYLoc();
 		int xLoc = m.getXLoc();
 		double yVel = m.getYVel();
@@ -33,5 +35,34 @@ public class MoverTest {
 		assertEquals(yLoc + yVel + yAcc, m.getYLoc(), 0);
 		assertEquals(xVel, m.getXVel(), 0);
 	}
-
+	@Test
+	public void updateTest2(){
+		Board b = new Board(100,100);
+		Mover m = new Player(b);
+		m.setStarted(false);
+		assertFalse(m.getStarted());
+		int yLoc = m.getYLoc();
+		int xLoc = m.getXLoc();
+		double yVel = m.getYVel();
+		double xVel = m.getXVel();
+		double yAcc = m.getYAcc();
+		m.update();
+		// not started
+		assertEquals(xLoc, m.getXLoc());
+		assertEquals(yLoc, m.getYLoc());
+		assertEquals(yVel, m.getYVel(), 0);
+		assertEquals(xVel, m.getXVel(), 0);
+		m.setStarted(true);
+		m.update();
+		System.out.println(m.getYLoc() + " " + m.getYVel() + " " + m.getYAcc());
+		// started
+		assertEquals(xLoc + xVel, m.getXLoc(), 0);
+		assertEquals(0, m.getYVel(), 0);
+		//assertEquals(yLoc + m.getYVel() + m.getYAcc(), m.getYLoc(), 0);
+		assertEquals(xVel, m.getXVel(), 0);
+		
+		m.setYLoc(0);
+		m.update();
+	}
+	
 }
