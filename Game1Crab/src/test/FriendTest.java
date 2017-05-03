367db1af8239d48be4e3c9418d0e369ad1264200
Chris Clayton <chrisbme@udel.edu>
2017-05-03 10:31:18 -0400

    Updates to Game1Crab FriendTest

--- Game1Crab/src/test/FriendTest.java
+++ Game1Crab/src/test/FriendTest.java
@@ -13,6 +13,9 @@ public class FriendTest {
 		assertEquals(f.getXLoc(), 1000);
 		assertEquals(f.getYLoc(), 1086);
 		//TODO: test yAcc, yVel, xVel
+		assertTrue(f.getXVel() == -18);
+		assertTrue(f.getYVel() == 0);
+		assertTrue(f.getYAcc() == 0);
 		
 	}
 }
