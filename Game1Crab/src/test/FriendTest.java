68acdcb7bae830ed0e1f0fa2cd00119a0fad0339
Chris Clayton <chrisbme@udel.edu>
2017-05-03 10:15:49 -0400

    Updated Crab 1 FriendTest

--- Game1Crab/src/test/FriendTest.java
+++ Game1Crab/src/test/FriendTest.java
@@ -8,7 +8,10 @@ import model.Board;
 public class FriendTest {
 	@Test
 	public void test(){
-		Board b = new Board(100,100);
+		Board b = new Board(1000,1200);
 		Friend f = new Friend(b);
+		assertEquals(f.getXLoc(), 1000);
+		assertEquals(f.getYLoc(), 1086);
+		//test yAcc and xAcc
 	}
 }
