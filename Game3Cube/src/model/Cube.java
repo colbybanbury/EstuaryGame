6ad597afecd3d9b89abc1408d035fc653ddbfe5a
Collin Clark <crclark96@gmail.com>
2017-05-03 14:48:34 -0400

    add game3 model javadoc

--- Game3Cube/src/model/Cube.java
+++ Game3Cube/src/model/Cube.java
@@ -13,6 +13,11 @@ public class Cube {
 	Random rand = new Random();
 	Board board;
 	
+	/**
+	 * Constructor for this class
+	 * @param picNum  integer value specifying which picture to display
+	 * @param b		  board the cube has to stay within
+	 */
 	public Cube(int picNum, Board b){
 		this.cubeNum = cubeNum;
 		this.board = b;
@@ -23,15 +28,26 @@ public class Cube {
 		*/
 		location = new Rectangle(picNum * board.width / board.NUM_CUBES, board.height / 2, sideLength, sideLength);
 	}
+	/**
+	 * Moves cube to random location within Board, assigns new picNum
+	 * (as if rolling a real cube on a board)
+	 */
 	public void roll(){
 		picNum = rand.nextInt(6);
 		location.setLocation(rand.nextInt(board.getWidth() - sideLength),
 							rand.nextInt(board.getHeight()) - sideLength);
 	}
+	/**
+	 * returns the current side of the cube being shown
+	 * @return picNum
+	 */
 	public int getPicNum(){
 		return picNum;
 	}
-	
+	/**
+	 * returns the location occupied by the cube
+	 * @return location
+	 */
 	public Rectangle getLocation(){
 		return location;
 	}
