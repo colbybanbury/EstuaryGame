e27465b8fd1f0c85c69f1eedf859342e2a2f0053
Chris Clayton <chrisbme@udel.edu>
2017-05-06 16:56:03 -0400

    friendCounter Tests added to FriendTest

--- Game1Crab/src/test/FriendTest.java
+++ Game1Crab/src/test/FriendTest.java
@@ -15,6 +15,34 @@ public class FriendTest {
 		assertEquals(f.getXVel(), -14, 0.000);
 		assertEquals(f.getYVel(), 0, 0.000);
 		assertEquals(f.getYAcc(), 0, 0.000);
+		assertEquals(f.friendCounter, 0);
 		
+	}
+	
+	@Test
+	public void friendCounterTest1(){ //friendCounter == 0
+		Board b = new Board(1000, 1200);
+		Friend f = new Friend(b);
+		f.setFriendCounter(0);
+		assertEquals(f.friendCounter, 0);
+		assertEquals(f.getFriendCounter(), f.friendCounter);
+	}
+	
+	@Test
+	public void friendCounterTest2(){ //friendCounter > 0
+		Board b = new Board(1000, 1200);
+		Friend f = new Friend(b);
+		f.setFriendCounter(10);
+		assertEquals(f.friendCounter, 10);
+		assertEquals(f.getFriendCounter(), f.friendCounter);
+	}
+	
+	@Test
+	public void friendCounterTest3(){ //friendCounter < 0
+		Board b = new Board(1000, 1200);
+		Friend f = new Friend(b);
+		f.setFriendCounter(-10);
+		assertEquals(f.friendCounter, -10);
+		assertEquals(f.getFriendCounter(), f.friendCounter);
 	}
 }
