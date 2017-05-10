31c7ea19583322e6bd410d11062504ec86428313
Collin Clark <crclark96@gmail.com>
2017-05-10 14:45:22 -0400

    update enemy test with new values

--- Game1Crab/src/test/EnemyTest.java
+++ Game1Crab/src/test/EnemyTest.java
@@ -13,8 +13,9 @@ public class EnemyTest {
 		Board b = new Board(1000,1200);
 		Enemy e = new Enemy(b);
 		assertEquals(e.getXLoc(), 1000);
-		assertTrue((e.getYLoc() >= 250) && (e.getYLoc() <= 750));
-		assertEquals(e.getXVel(), -18, 0.000);
+		assertTrue((e.getYLoc() >= .25*1200) && (e.getYLoc() <= .75*1200));
+		assertTrue(e.getXVel() < -13);
+		assertTrue(e.getXVel() > -23); 
 		assertEquals(e.getYVel(), 0, 0.000);
 		assertEquals(e.getYAcc(), 0, 0.000);
 	}
