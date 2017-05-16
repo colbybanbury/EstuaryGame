8b793386f42535426ebb0a00a01c0672eb54f053
Collin Clark <crclark96@gmail.com>
2017-05-16 01:03:41 -0400

    add basic animation test

---
+++ FinalBuild/test/game3/test/AnimationTest.java
@@ -0,0 +1,18 @@
+package game3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game3.model.Board;
+import game3.view.Animation;
+
+public class AnimationTest {
+
+	@Test
+	public void test() {
+		Animation a = new Animation(new Board(1000,1000));
+		
+	}
+
+}
