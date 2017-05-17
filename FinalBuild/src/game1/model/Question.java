a43e34eb7cd5734188e6a9d9ec84394185b154ea
Collin Clark <crclark96@gmail.com>
2017-05-16 21:21:09 -0400

    add javadocs to Question

--- FinalBuild/src/game1/model/Question.java
+++ FinalBuild/src/game1/model/Question.java
@@ -3,11 +3,21 @@ package game1.model;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * This class creates the questions and answers that appear when the player collides with an enemy
+ */
 public class Question {
 	private String prompt;
 	private String correctAnswer;
 	private ArrayList<String> answers;
 	
+	/**
+	 * Creates instance of Question
+	 * @param p prompt to display at top of frame
+	 * @param cA the correct answer (if the corresponding button is selected, the game will continue)
+	 * @param wA1 an incorrect answer (selecting the corresponding button will not progress the game)
+	 * @param wA2 an incorrect answer (selecting the corresponding button will not progress the game)
+	 */
 	public Question(String p, String cA, String wA1, String wA2){
 		this.prompt = p;
 		this.correctAnswer = cA;
@@ -21,10 +31,18 @@ public class Question {
 		shuffle();
 	}
 	
+	/**
+	 * shuffles the answer so they will appear in a random order when the prompt appears
+	 */
 	public void shuffle(){
 		Collections.shuffle(this.answers);
 	}
 	
+	/**
+	 * determines if answer selected is correct
+	 * @param ans answer to test
+	 * @return boolean
+	 */
 	public boolean isRightAnswer(String ans){
 		return(ans.equals(correctAnswer));
 	}
